0a13e842c875c131f7ec273a5f4b4ee1ef26816e
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-02-25 22:05:04 -0500

    minor changes and new build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: './',
   plugins: [react(), viteTsconfigPaths()],
   server: {
     open: true,
