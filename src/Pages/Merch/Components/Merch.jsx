def71809a64028547b0959d0ac8f78bce5ea29a5
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-03-02 02:30:00 -0500

    props for merch list

--- src/Pages/Merch/Components/Merch.jsx
+++ src/Pages/Merch/Components/Merch.jsx
@@ -5,10 +5,16 @@ import MerchGallery from './MerchGallery';
 import axios from 'axios';
 
 export default function Merch() {
+    var betterMerchList = [{category: "Unisex T-Shirt, S-L", name: "2022 Barrio T-Shirt", price: '$25.00', imgPath: "/Images/Leadership/Leadership_Title_35B&C.jpg"},
+    {category: 'Unisex T-Shirt, S-L', name: '2022 Barrio T-Shirt', price: '$25.00', imgPath: '/Images/Leadership/Leadership_Title_35B&C.jpg'},
+    {category: "Unisex Hoodie, XS-XL", name: "Super duper awesome hoodie", price: '$35.00', imgPath: "/Images/Leadership/Leadership_Title_35B&C.jpg"},
+    {category: "Unisex T-Shirt, S-L", name: "2022 Barrio T-Shirt", price: '$25.00', imgPath: "/Images/Leadership/Leadership_Title_35B&C.jpg"},
+    {category: "Unisex T-Shirt, S-L", name: "2022 Barrio T-Shirt", price: '$25.00', imgPath: "/Images/Leadership/Leadership_Title_35B&C.jpg"},]
+
     return(
         <>
             <HalfTitle header = 'Merch' imgSrc = {'/Images/Leadership/Leadership_Title_35B&C.jpg'} brightness={75} position={25} caption='Order Now!'/>
-            <MerchGallery/>
+            <MerchGallery merch={betterMerchList}/>
         </>
     )
 }
--- src/Pages/Merch/Components/MerchGallery.jsx
+++ src/Pages/Merch/Components/MerchGallery.jsx
@@ -1,13 +1,12 @@
 import styles from '../Stylesheets/MerchGallery.module.css'
 
 export default function MerchGallery({ merch }) {
-    var merchList = [1,2,3,4,5,6];
-    merch = merchList;
     return (
         <div className={styles.merch_grid_container}>
+            <p className={styles.merch_gallery_title}>Collection</p>
             <div className={styles.merch_grid}>
                 {merch.map((merchItem, index) => (
-                    <MerchItem key={index}/>
+                    <MerchItem key={index} item={merchItem}/>
                 ))}
             </div>
         </div>
@@ -18,11 +17,11 @@ function MerchItem({ item }) {
     return (
         <div className={styles.merch_container} style={{ cursor: 'pointer' }}> {/* Remember: This is the button (not the img) */}
             <div className={styles.merch_img_container}>
-                <img src={'/Images/Leadership/Leadership_Title_35B&C.jpg'} />
+                <img src={item.imgPath} />
             </div>
-            <p className={styles.merch_category}>UNISEX T-SHIRT</p>
-            <p className={styles.merch_name}>hellllllllllllllllllllllllllllllllllaaaaaaaaaaa     el el el el el el el ellllll el el </p>
-            <p className={styles.merch_price}>$25.00</p>
+            <p className={styles.merch_category}>{item.category.toUpperCase()}</p>
+            <p className={styles.merch_name}>{item.name}</p>
+            <p className={styles.merch_price}>{item.price}</p>
         </div>
     );
 }
