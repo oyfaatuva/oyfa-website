72733483c80ae973f71c776bf0a42b7d222a37dd
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-03-04 22:32:47 -0500

    Updated packages, installed react-slick

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { createBrowserRouter, RouterProvider, ScrollRestoration } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './App.css'
 
 import Home from './pages/Home/Components/home'
