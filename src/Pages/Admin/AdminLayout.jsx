6b9f548a808fb4e5683ef36effb1d1510da715fe
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-03-15 15:44:18 -0400

    Fixed bug with AdminLayout not redirecting to login and replacing history properly

--- src/Pages/Admin/AdminLayout.jsx
+++ src/Pages/Admin/AdminLayout.jsx
@@ -1,17 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
 
 /* This is a Layout that allows only authenticated users to its children in the Router (see App.jsx) */
 
 export default function AdminLayout() {
+    const navigate = useNavigate();
     const { user } = useAuth();
 
-    if (!user) {
-        return <Navigate to="/admin/login"/>;
-    }
+    useEffect(() => {
+        if (!user) {
+            /* { replace: true } so that login pages replaces whatever page unauthenticated users were trying to access in history
+            avoiding loop when clicking back arrow */ 
+            navigate('/admin/login', {replace: true})
+        }
+    })
     
     return(            
         <Outlet/>
     );
 }
+
