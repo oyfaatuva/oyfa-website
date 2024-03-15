b731d68bc08797173cca7bed56e2853e8ea1c25d
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-03-14 23:24:00 -0400

    Admin Merch Component

--- src/Pages/Admin/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,4 +1,5 @@
 import { useAuth } from "../../../hooks/useAuth";
+import AdminMerch from "../AdminMerch/AdminMerch";
 
 export default function AdminDashboard () {
     const { logout } = useAuth();
@@ -11,6 +12,7 @@ export default function AdminDashboard () {
         <>
             <p>This is my dashboard</p>
             <button onClick={handleLogout}>Logout</button>
+            <AdminMerch/>
         </>
     );
 }
