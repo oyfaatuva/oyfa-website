100e44c7237b83980941d9b2ec496c9ea803767f
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-03-18 16:03:03 -0400

    Cleaned HalfTitle  code responsible for dynamic style and added appearing div

--- src/components/layout/HalfTitle/HalfTitle.jsx
+++ src/components/layout/HalfTitle/HalfTitle.jsx
@@ -1,8 +1,9 @@
-import {Component} from 'react'
 import { Link } from 'react-router-dom';
-import './HalfTitle.css'
 import Vignette from '../../ui/Vignette/Vignette';
 import FadeOnLoadImg from '../../ui/FadeOnLoadImg/FadeOnLoadImg';
+import AppearingDiv from '../../ui/AppearingText/AppearingDiv';
+
+import './HalfTitle.css'
 
 /* This is a common title panel which occupies 50% screen height; appears on all pages except Home.
 
@@ -25,38 +26,24 @@ import FadeOnLoadImg from '../../ui/FadeOnLoadImg/FadeOnLoadImg';
 **      component.
 **==============================================================================================================*/
 
-export default class HalfTitle extends Component{
-    render(){
-        /* default prop values declared at bottom of file */
+export default function HalfTitle ({header, imgSrc, brightness, position, caption, captionLink, newTab}) {
+    /* default prop values declared at bottom of file */
 
-        /* Because these CSS styles can change depending on the props, we have to use JS to dynamically set them */
-        const brightnessStyle = `
-            .half_title_img_brightness{
-                filter: brightness(${this.props.brightness}%);
-            }`
-
-        const positionStyle = `
-            .half_title_img_position{
-                object-position: center ${this.props.position}%;
-            }`
-
-        var newTab = ''
-        if(this.props.newTab == 'true') newTab = '_blank'
-        
-        return(
-            <div className = 'half_title_container'>
-                <style>{brightnessStyle}</style>
-                <style>{positionStyle}</style>
-                <FadeOnLoadImg imgPath={this.props.imgSrc} className='half_title_img half_title_img_brightness half_title_img_position'/>
-                {/* <img src = {this.props.imgSrc} className = 'half_title_img half_title_img_brightness half_title_img_position fade_in_image'/> */}
-                <Vignette/>
-                <h1 className ='half_title_heading'>{this.props.header}</h1>
-                <Link to={this.props.captionLink} target={newTab}>
-                    <p className='half_title_caption'>{this.props.caption}</p>
+    /* Because these CSS styles can change depending on the props, we have to use JS to dynamically set them */
+    const imageStyle = { filter: `brightness(${brightness}%)`, objectFit: 'center', objectPosition: `${position}%`}
+    
+    return(
+        <div className = 'half_title_container'>
+            <FadeOnLoadImg imgPath={imgSrc} className='half_title_img' style={imageStyle}/>
+            <Vignette/>
+            <AppearingDiv>
+                <h1 className ='half_title_heading'>{header}</h1>
+                <Link to={captionLink} target={newTab ? '_blank' : ''}>
+                    <p className='half_title_caption'>{caption}</p>
                 </Link>
-            </div>
-        )
-    }
+            </AppearingDiv>
+        </div>
+    )
 }
 
 HalfTitle.defaultProps = {
