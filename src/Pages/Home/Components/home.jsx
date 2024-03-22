772c9f853e12cab32f9ea59a5e8a3b7e9b9465f7
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-03-22 10:05:27 -0400

    Home Title fills view port with a minimum using useWindowDim hook

--- src/Pages/Home/Components/home.jsx
+++ src/Pages/Home/Components/home.jsx
@@ -3,13 +3,15 @@ import { Helmet } from 'react-helmet';
 import HomeTitle from './homeTitle';
 import Introduction from './homeIntro';
 import Testimonies from './homeTestimonies';
+import useWindowDimensions from './../../../hooks/useWindowDimensions';
 
 /* Main export file to index that combines all "home" components */
 
 export default function Home () {
     const {setNavbarScrollPosition, setNavbarScrollPositionMobile} = useOutletContext();
+    const {height} = useWindowDimensions(); 
     
-    setNavbarScrollPosition(740);
+    setNavbarScrollPosition(height - 70);
     setNavbarScrollPositionMobile(430);
 
     return(
