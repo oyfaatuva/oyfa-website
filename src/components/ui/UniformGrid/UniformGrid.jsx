a3cfecb63083008871a75ac186181e9a958ca34d
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-03-28 14:07:20 -0400

    Seperated UniformGrid to grid and item comp, grid gap now prop

--- src/components/ui/UniformGrid/UniformGrid.jsx
+++ src/components/ui/UniformGrid/UniformGrid.jsx
@@ -1,28 +1,19 @@
 import styles from './UniformGrid.module.css'; 
 
-export default function UniformGrid() {
-    const items = [
-        { id: 1, name: 'Item 1' },
-        { id: 2, name: 'Item 2' },
-        { id: 3, name: 'Item 3' },
-        { id: 4, name: 'Item 4' },
-        { id: 5, name: 'Item 5' },
-        { id: 6, name: 'Item 6' },
-        { id: 7, name: 'Item 7' },
-        { id: 8, name: 'Item 8' },
-        { id: 9, name: 'Item 9' },
-        { id: 10, name: 'Item 10' },
-        { id: 11, name: 'Item 11' },
-        { id: 12, name: 'Item 12' },
-    ];
-
+export default function UniformGrid( {gridGap = 20, children} ) {
     return (
-        <div className={styles.grid_container}>
-            {items.map(item => (
-                <div key={item.id} className={styles.grid_item}>
-                    {item.name}
-                </div>
-            ))}
+        <div className={styles.grid_container} style={{gridGap: `${gridGap}px`}}> 
+            {children}
         </div>
     );
 }
+
+const Item = ({ children }) => {
+    return (
+        <div className={styles.grid_item}>
+            {children}
+        </div>
+    );
+}
+
+UniformGrid.Item = Item;
