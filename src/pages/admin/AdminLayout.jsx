f84266c67879308c6e2a8c2df3b6aba0f14af1ab
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-03-28 10:23:57 -0400

    AdminLayout validates token now in useEffect

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -1,6 +1,7 @@
+import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../../components/AuthContext";
-import { useEffect } from "react";
+import axiosClient from './../../utils/axiosClient';
 
 /* This is a Layout that allows only authenticated users to its children in the Router (see App.jsx) */
 
@@ -8,11 +9,21 @@ export default function AdminLayout() {
     const navigate = useNavigate();
     const { token } = useAuth();
 
+    async function validateToken() {
+        try {
+            await axiosClient.get('/auth/validateToken');
+        } catch (error) {
+            console.error('Token validation failed:', error);
+            navigate('/admin/login', { replace: true });
+        }
+    }
+
     useEffect(() => {
-        // TODO: add message when logged state changes and logged off 
+        // TODO: add message when logged state changes and logged off
+        validateToken();
         if (!token) 
             navigate('/admin/login', {replace: true})
-    })
+    }, [token])
     
     return(            
         <Outlet/>
