5ec54e89332006a80d84a8afb24049ce6f90e8e7
Sebastian Borromeo <uwg3xs@virginia.edu>
2024-04-19 01:32:23 -0400

    set vite config to split node modules in built

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
-  plugins: [react()],
-  server: {
-    open: true,
-    port: 3000,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup'
-  }
+    base: './',
+    plugins: [react()],
+    server: {
+        open: true,
+        port: 3000,
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/tests/setup'
+    },
+    build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
